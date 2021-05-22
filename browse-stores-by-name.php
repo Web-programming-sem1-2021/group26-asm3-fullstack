8720fb8153ffeab79b156a67f6fa960988f0168b
giangnguyen <giang.nguyen@skedulo.com>
2021-05-22 22:10:27 +0700

    Add some initial solutions

--- browse-stores-by-name.php
+++ browse-stores-by-name.php
@@ -2,358 +2,100 @@
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Browse</title>
-  <link rel="stylesheet" href="../../style.css" />
-  <link rel="stylesheet" href="../../style/browse.css" />
-  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" />
-  <script src="../../jsFunctions.js" defer></script>
-  <link rel="stylesheet" href="/style/cookie.css">
-  <script src="/script/cookie.js" defer></script>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Browse</title>
+    <link rel="stylesheet" href="../../style.css" />
+    <link rel="stylesheet" href="../../style/browse.css" />
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" />
+    <script src="../../jsFunctions.js" defer></script>
+    <link rel="stylesheet" href="/style/cookie.css">
+    <script src="/script/cookie.js" defer></script>
 </head>
 
 <body>
-  <!-- referenced from : "https://www.w3schools.com/howto/howto_js_responsive_navbar_dropdown.asp" -->
-  <?php
+    <!-- referenced from : "https://www.w3schools.com/howto/howto_js_responsive_navbar_dropdown.asp" -->
+    <?php
 
   include('inc/header.php');
 
   ?>
 
-  <h1 style="margin: 20px; color: gray; font-size: 50px">
-    Browse Stores by Name
-  </h1>
-  <aside>
-    <section>
-      <section class="row">
-        <section class="column">
-          <label class="container">A
-            <input type="checkbox" checked="checked" />
-            <span class="checkmark"></span>
-          </label>
-          <label class="container">B
-            <input type="checkbox" />
-            <span class="checkmark"></span>
-          </label>
-          <label class="container">C
-            <input type="checkbox" />
-            <span class="checkmark"></span>
-          </label>
-          <label class="container">D
-            <input type="checkbox" />
-            <span class="checkmark"></span>
-          </label>
-          <label class="container">E
-            <input type="checkbox" />
-            <span class="checkmark"></span>
-          </label>
-          <label class="container">F
-            <input type="checkbox" />
-            <span class="checkmark"></span>
-          </label>
-          <label class="container">G
-            <input type="checkbox" />
-            <span class="checkmark"></span>
-          </label>
-          <label class="container">H
-            <input type="checkbox" />
-            <span class="checkmark"></span>
-          </label>
-          <label class="container">I
-            <input type="checkbox" />
-            <span class="checkmark"></span>
-          </label>
-          <label class="container">J
-            <input type="checkbox" />
-            <span class="checkmark"></span>
-          </label>
-          <label class="container">K
-            <input type="checkbox" />
-            <span class="checkmark"></span>
-          </label>
-          <label class="container">L
-            <input type="checkbox" />
-            <span class="checkmark"></span>
-          </label>
-          <label class="container">M
-            <input type="checkbox" checked="checked" />
-            <span class="checkmark"></span>
-          </label>
-        </section>
-        <section class="column">
-          <label class="container">N
-            <input type="checkbox" />
-            <span class="checkmark"></span>
-          </label>
-          <label class="container">0
-            <input type="checkbox" />
-            <span class="checkmark"></span>
-          </label>
-          <label class="container">P
-            <input type="checkbox" />
-            <span class="checkmark"></span>
-          </label>
-          <label class="container">Q
-            <input type="checkbox" />
-            <span class="checkmark"></span>
-          </label>
-          <label class="container">R
-            <input type="checkbox" />
-            <span class="checkmark"></span>
-          </label>
-          <label class="container">S
-            <input type="checkbox" />
-            <span class="checkmark"></span>
-          </label>
-          <label class="container">T
-            <input type="checkbox" />
-            <span class="checkmark"></span>
-          </label>
-          <label class="container">U
-            <input type="checkbox" />
-            <span class="checkmark"></span>
-          </label>
-          <label class="container">V
-            <input type="checkbox" />
-            <span class="checkmark"></span>
-          </label>
-          <label class="container">W
-            <input type="checkbox" />
-            <span class="checkmark"></span>
-          </label>
-          <label class="container">X
-            <input type="checkbox" />
-            <span class="checkmark"></span>
-          </label>
-          <label class="container">Y
-            <input type="checkbox" />
-            <span class="checkmark"></span>
-          </label>
-          <label class="container">Z
-            <input type="checkbox" />
-            <span class="checkmark"></span>
-          </label>
-        </section>
-      </section>
-    </section>
-  </aside>
-  <div class="store-cards">
-    <div class="store-card">
-      <a href="../../storepages/store-2.html">
-        <h2>Store ne</h2>
-      </a>
-      <p>
-        <a href="../../storepages/store-2.html">
-          <img class="store-image" src="../../storepages/images/Jacket/jacket5.jpg" alt="Picture of Helen" />
-        </a>
-      </p>
-      <p class="thumbnail-description">
-        Helen has fourth grade piano but in secret prefers to play electronic
-        keyboards. She loves synthesizer sounds and arguing with Jean over who
-        should play the basslines.
-      </p>
-    </div>
-    <div class="store-card">
-      <a href="../../storepages/store-1.html">
-        <h2>Store name</h2>
-      </a>
-      <p>
-        <a href="../../storepages/store-1.html">
-          <img class="store-image" src="../../storepages/images/laptop.png" alt="Picture of Helen" />
-        </a>
-      </p>
-      <p class="thumbnail-description">
-        Helen has fourth grade piano but in secret prefers to play electronic
-        keyboards. She loves synthesizer sounds and arguing with Jean over who
-        should play the basslines.
-      </p>
-    </div>
-    <div class="store-card">
-      <a href="../../storepages/store-2.html">
-        <h2>Store name</h2>
-      </a>
-      <p>
-        <a href="../../storepages/store-2.html">
-          <img class="store-image" src="../../storepages/images/Jacket/jacket5.jpg" alt="Picture of Helen" />
-        </a>
-      </p>
-      <p class="thumbnail-description">
-        Helen has fourth grade piano but in secret prefers to play electronic
-        keyboards. She loves synthesizer sounds and arguing with Jean over who
-        should play the basslines.
-      </p>
-    </div>
-    <div class="store-card">
-      <a href="../../storepages/store-1.html">
-        <h2>Store name</h2>
-      </a>
-      <p>
-        <a href="../../storepages/store-1.html">
-          <img class="store-image" src="../../storepages/images/laptop.png" alt="Picture of Helen" />
-        </a>
-      </p>
-      <p class="thumbnail-description">
-        Helen has fourth grade piano but in secret prefers to play electronic
-        keyboards. She loves synthesizer sounds and arguing with Jean over who
-        should play the basslines.
-      </p>
-    </div>
-    <div class="store-card">
-      <a href="../../storepages/store-1.html">
-        <h2>Store name</h2>
-      </a>
-      <p>
-        <a href="../../storepages/store-1.html">
-          <img class="store-image" src="../../storepages/images/laptop.png" alt="Picture of Helen" />
-        </a>
-      </p>
-      <p class="thumbnail-description">
-        Helen has fourth grade piano but in secret prefers to play electronic
-        keyboards. She loves synthesizer sounds and arguing with Jean over who
-        should play the basslines.
-      </p>
-    </div>
-    <div class="store-card">
-      <a href="../../storepages/store-2.html">
-        <h2>Store name</h2>
-      </a>
-      <p>
-        <a href="../../storepages/store-2.html">
-          <img class="store-image" src="../../storepages/images/Jacket/jacket5.jpg" alt="Picture of Helen" />
-        </a>
-      </p>
-      <p class="thumbnail-description">
-        Helen has fourth grade piano but in secret prefers to play electronic
-        keyboards. She loves synthesizer sounds and arguing with Jean over who
-        should play the basslines.
-      </p>
-    </div>
-    <div class="store-card">
-      <a href="../../storepages/store-1.html">
-        <h2>Store name</h2>
-      </a>
-      <p>
-        <a href="../../storepages/store-1.html">
-          <img class="store-image" src="../../storepages/images/laptop.png" alt="Picture of Helen" />
-        </a>
-      </p>
-      <p class="thumbnail-description">
-        Helen has fourth grade piano but in secret prefers to play electronic
-        keyboards. She loves synthesizer sounds and arguing with Jean over who
-        should play the basslines.
-      </p>
-    </div>
-    <div class="store-card">
-      <a href="../../storepages/store-2.html">
-        <h2>Store name</h2>
-      </a>
-      <p>
-        <a href="../../storepages/store-2.html">
-          <img class="store-image" src="../../storepages/images/Jacket/jacket5.jpg" alt="Picture of Helen" />
-        </a>
-      </p>
-      <p class="thumbnail-description">
-        Helen has fourth grade piano but in secret prefers to play electronic
-        keyboards. She loves synthesizer sounds and arguing with Jean over who
-        should play the basslines.
-      </p>
-    </div>
-    <div class="store-card">
-      <a href="../../storepages/store-2.html">
-        <h2>Store name</h2>
-      </a>
-      <p>
-        <a href="../../storepages/store-2.html">
-          <img class="store-image" src="../../storepages/images/Jacket/jacket5.jpg" alt="Picture of Helen" />
-        </a>
-      </p>
-      <p class="thumbnail-description">
-        Helen has fourth grade piano but in secret prefers to play electronic
-        keyboards. She loves synthesizer sounds and arguing with Jean over who
-        should play the basslines.
-      </p>
-    </div>
-    <div class="store-card">
-      <a href="../../storepages/store-1.html">
-        <h2>Store name</h2>
-      </a>
-      <p>
-        <a href="../../storepages/store-1.html">
-          <img class="store-image" src="../../storepages/images/laptop.png" alt="Picture of Helen" />
-        </a>
-      </p>
-      <p class="thumbnail-description">
-        Helen has fourth grade piano but in secret prefers to play electronic
-        keyboards. She loves synthesizer sounds and arguing with Jean over who
-        should play the basslines.
-      </p>
-    </div>
-    <div class="store-card">
-      <a href="../../storepages/store-2.html">
-        <h2>Store name</h2>
-      </a>
-      <p>
-        <a href="../../storepages/store-2.html">
-          <img class="store-image" src="../../storepages/images/Jacket/jacket5.jpg" alt="Picture of Helen" />
-        </a>
-      </p>
-      <p class="thumbnail-description">
-        Helen has fourth grade piano but in secret prefers to play electronic
-        keyboards. She loves synthesizer sounds and arguing with Jean over who
-        should play the basslines.
-      </p>
-    </div>
-    <div class="store-card">
-      <a href="../../storepages/store-1.html">
-        <h2>Store name</h2>
-      </a>
-      <p>
-        <a href="../../storepages/store-1.html">
-          <img class="store-image" src="../../storepages/images/laptop.png" alt="Picture of Helen" />
-        </a>
-      </p>
-      <p class="thumbnail-description">
-        Helen has fourth grade piano but in secret prefers to play electronic
-        keyboards. She loves synthesizer sounds and arguing with Jean over who
-        should play the basslines.
-      </p>
-    </div>
-  </div>
+    <h1 style="margin: 20px; color: gray; font-size: 50px">
+        Browse Stores by Name
+    </h1>
+    <aside>
 
-  <footer>
-    <div class="footer">
-      <div class="footer-content">
-        <div class="footer-section">
-          <h3>Other Authorities</h3>
-          <h3>
-            <a href="/homepage/footer/copyright.html"> Copyright</a>
-          </h3>
-          <h3><a href="/homepage/footer/privacy.html">Privacy Policy</a></h3>
-          <h3><a href="/homepage/footer/terms.html">Terms of Service</a></h3>
-        </div>
-        <div class="footer-section">
-          <h3>Student contributors</h3>
-          <ol>
-            <li>Nguyen Thi Quynh Giang <b>[s3866617]</b></li>
+        <?php
+
+    function csvToJson($fname)
+    {
+      if (!($fp = fopen($fname, 'r'))) {
+        die("Can't open file...");
+      }
+      $key = fgetcsv($fp, "1024", ",");
+      $json = array();
+      while ($row = fgetcsv($fp, "1024", ",")) {
+        $json[] = array_combine($key, $row);
+      }
+      fclose($fp);
+      return json_encode($json, true);
+    }
+
+    $fname = './data/categories.csv';
+    $store = './data/stores.csv';
+
+    $json = csvToJson($fname);
+    $a = json_decode($json);
+
+    $storeCSV = json_decode(csvToJson($store))
+    ?>
+        <section>
+            <?php
+      for ($index = 0; $index <= count($a); $index++) {
+      ?>
+            <a
+                href="/browse-stores-by-name.php?id=<?php echo $a[$index]->id ?>"><?php echo $a[$index]->name ?></a><br />
             <br />
-            <li>Joo Jeong-hyeon<b>[s3865746]</b></li>
-            <br />
-            <li>Vo Khai Minh<b>[s3879953]</b></li>
-            <br />
-            <li>Ramanaharan Ramcharan<b>[s3775271]</b></li>
-          </ol>
+            <?php } ?>
+        </section>
+        </section>
+    </aside>
+    <div class="store-cards">
+        <?php
+    for ($i = 0; $i <= count($storeCSV); $i++) {
+      if (
+        $storeCSV[$i]->category_id === $_GET['id']
+      ) {
+    ?>
+        <div class="store-card">
+            <a href="../../storepages/store-2.html">
+                <h2>
+                    <?php
+
+              echo $storeCSV[$i]->name;
+              ?>
+                </h2>
+            </a>
+            <p>
+                <a href="../../storepages/store-2.html">
+                    <img class="store-image" src="../../storepages/images/Jacket/jacket5.jpg" alt="Picture of Helen" />
+                </a>
+            </p>
+            <p class="thumbnail-description">
+                Helen has fourth grade piano but in secret prefers to play electronic
+                keyboards. She loves synthesizer sounds and arguing with Jean over who
+                should play the basslines.
+            </p>
         </div>
-        <div class="footer-section">
-          <h3>Application Information</h3>
-          <a href="https://github.com/Web-programming-sem1-2021/Group26_Asm2_Javascript"><img class="footer-icon" src="../../icons/github-character.svg" />
-            Source Code</a>
-          <a href="https://web-programming-sem1-2021.github.io/Group26_Asm2_Javascript/"><img class="footer-icon" src="../../icons/footerIcons/globe.svg" />
-            Website</a>
-        </div>
-      </div>
+        <?php }
+    } ?>
+
     </div>
-  </footer>
+
+    <footer>
+        <?php include('inc/footer.php'); ?>
+    </footer>
 </body>
 
 </html>
