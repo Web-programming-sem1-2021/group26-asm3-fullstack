cf2e8cecb2ae7aa82e14f7d8a04fd78a996769d5
giangnguyen <giang.nguyen@skedulo.com>
2021-05-22 22:22:01 +0700

    handle id = null in browse

--- browse-stores-by-name.php
+++ browse-stores-by-name.php
@@ -17,9 +17,9 @@
     <!-- referenced from : "https://www.w3schools.com/howto/howto_js_responsive_navbar_dropdown.asp" -->
     <?php
 
-  include('inc/header.php');
+    include('inc/header.php');
 
-  ?>
+    ?>
 
     <h1 style="margin: 20px; color: gray; font-size: 50px">
         Browse Stores by Name
@@ -28,68 +28,91 @@
 
         <?php
 
-    function csvToJson($fname)
-    {
-      if (!($fp = fopen($fname, 'r'))) {
-        die("Can't open file...");
-      }
-      $key = fgetcsv($fp, "1024", ",");
-      $json = array();
-      while ($row = fgetcsv($fp, "1024", ",")) {
-        $json[] = array_combine($key, $row);
-      }
-      fclose($fp);
-      return json_encode($json, true);
-    }
+        function csvToJson($fname)
+        {
+            if (!($fp = fopen($fname, 'r'))) {
+                die("Can't open file...");
+            }
+            $key = fgetcsv($fp, "1024", ",");
+            $json = array();
+            while ($row = fgetcsv($fp, "1024", ",")) {
+                $json[] = array_combine($key, $row);
+            }
+            fclose($fp);
+            return json_encode($json, true);
+        }
 
-    $fname = './data/categories.csv';
-    $store = './data/stores.csv';
+        $fname = './data/categories.csv';
+        $store = './data/stores.csv';
 
-    $json = csvToJson($fname);
-    $a = json_decode($json);
+        $json = csvToJson($fname);
+        $a = json_decode($json);
 
-    $storeCSV = json_decode(csvToJson($store))
-    ?>
+        $storeCSV = json_decode(csvToJson($store))
+        ?>
         <section>
             <?php
-      for ($index = 0; $index <= count($a); $index++) {
-      ?>
-            <a
-                href="/browse-stores-by-name.php?id=<?php echo $a[$index]->id ?>"><?php echo $a[$index]->name ?></a><br />
-            <br />
+            for ($index = 0; $index <= count($a); $index++) {
+            ?>
+                <a href="/browse-stores-by-name.php?id=<?php echo $a[$index]->id ?>"><?php echo $a[$index]->name ?></a><br />
+                <br />
             <?php } ?>
         </section>
         </section>
     </aside>
     <div class="store-cards">
         <?php
-    for ($i = 0; $i <= count($storeCSV); $i++) {
-      if (
-        $storeCSV[$i]->category_id === $_GET['id']
-      ) {
-    ?>
-        <div class="store-card">
-            <a href="../../storepages/store-2.html">
-                <h2>
-                    <?php
+        for ($i = 0; $i <= count($storeCSV); $i++) {
 
-              echo $storeCSV[$i]->name;
-              ?>
-                </h2>
-            </a>
-            <p>
-                <a href="../../storepages/store-2.html">
-                    <img class="store-image" src="../../storepages/images/Jacket/jacket5.jpg" alt="Picture of Helen" />
-                </a>
-            </p>
-            <p class="thumbnail-description">
-                Helen has fourth grade piano but in secret prefers to play electronic
-                keyboards. She loves synthesizer sounds and arguing with Jean over who
-                should play the basslines.
-            </p>
-        </div>
-        <?php }
-    } ?>
+            if (
+                $_GET['id'] !== null &&
+                $storeCSV[$i]->category_id === $_GET['id']
+            ) {
+        ?>
+                <div class="store-card">
+                    <a href="../../storepages/store-2.html">
+                        <h2>
+                            <?php
+
+                            echo $storeCSV[$i]->name;
+                            ?>
+                        </h2>
+                    </a>
+                    <p>
+                        <a href="../../storepages/store-2.html">
+                            <img class="store-image" src="../../storepages/images/Jacket/jacket5.jpg" alt="Picture of Helen" />
+                        </a>
+                    </p>
+                    <p class="thumbnail-description">
+                        Helen has fourth grade piano but in secret prefers to play electronic
+                        keyboards. She loves synthesizer sounds and arguing with Jean over who
+                        should play the basslines.
+                    </p>
+                </div>
+            <?php } else if ($_GET['id'] === null) { ?>
+                <div class="store-card">
+                    <a href="../../storepages/store-2.html">
+                        <h2>
+                            <?php
+
+                            echo $storeCSV[$i]->name;
+                            ?>
+                        </h2>
+                    </a>
+                    <p>
+                        <a href="../../storepages/store-2.html">
+                            <img class="store-image" src="../../storepages/images/Jacket/jacket5.jpg" alt="Picture of Helen" />
+                        </a>
+                    </p>
+                    <p class="thumbnail-description">
+                        Helen has fourth grade piano but in secret prefers to play electronic
+                        keyboards. She loves synthesizer sounds and arguing with Jean over who
+                        should play the basslines.
+                    </p>
+                </div>
+        <?php
+            }
+        } ?>
 
     </div>
 
