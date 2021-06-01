7ead80600bd8396ef9ed34fa914b1256c2daefb5
JooJeonghyeon99 <s3865746@rmit.edu.vn>
2021-06-01 12:40:06 +0700

    Fix linking error

--- src/storepages/product-1.php
+++ src/storepages/product-1.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
@@ -16,7 +17,7 @@
 </head>
 
 <body>
-  <?php require '../inc/header.php' ?>
+  <?php include '../inc/header.php' ?>
 
   <div id="mySidenav" class="sidenav">
     <a href="javascript:void(0)" class="closebtn" onclick="closeNav()"><img src="./images/close.png" width="20px" /></a>
